3a937f9e16bbf22b4ace7e75c5a8867ca776674b
Jakub <jakub.moskal3@gmail.com>
2017-12-18 10:53:19 +0100

    generated files on 2017-12-18 10:53:19

--- src/pl/mosquito/chip8/emulator/Keyboard.java
+++ src/pl/mosquito/chip8/emulator/Keyboard.java
@@ -1,14 +1,32 @@
 package pl.mosquito.chip8.emulator;
 
-import java.awt.event.KeyAdapter;
 
-public class Keyboard extends KeyAdapter {
+import javafx.scene.input.KeyEvent;
+
+public class Keyboard  {
 
     static final int [] KeyCodeMap = {
-
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            0,
+            (int) 'a',
+            (int) 'b',
+            (int) 'f',
+            (int) 'e',
+            (int) 'd',
+            (int) 'c',
     };
 
     private int CurrentKey = 0;
+
+
 
     int getCurrentKey() {
         return CurrentKey;

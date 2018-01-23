6d13a1788c3dd17c4241ed8232cf48adad8ada45
Jakub <jakub.moskal3@gmail.com>
2018-01-23 20:02:06 +0100

    generated files on 2018-01-23 20:02:06

--- src/main/java/pl/mosquito/chip8/emulator/Memory.java
+++ src/main/java/pl/mosquito/chip8/emulator/Memory.java
@@ -4,6 +4,7 @@ import static pl.mosquito.chip8.emulator.Keyboard.KSIZE;
 import static pl.mosquito.chip8.emulator.Keyboard.KeyCode;
 
 public class Memory {
+
     //The default size of memory
 
     private final int MEMORY_4K = 0x1000; //4096
@@ -118,61 +119,103 @@ public class Memory {
                      */
                     case 0x0000:
                         scrClr();
-                    break;
+                        break;
                     /**
                      * Return from subroutine call
                      */
                     case 0x000E:
                         returnFromSubroutine();
-                    break;
+                        break;
                 }
                 /**
                  * jump to address NNN
                  */
-            case 0x1000:
+            case 0x1:
                 jumpToAddr(opcode);
-            break;
+                break;
             /**
              * 2NN-jump to subroutine at address NNN
              */
-            case 0x2000:
+            case 0x2:
                 jumpToSubroutineAddr(opcode);
-            break;
+                break;
             /**
              * 3XKK-skip next instruction if reguster VX == register KK
              */
-            case 0x3000:
+            case 0x3:
                 skipInstr("3XKK");
-            break;
+                break;
             /**
              * 4XKK-skip next instruction if reguster VX != register KK
              */
-            case 0x4000:
+            case 0x4:
                 skipInstr("4XKK");
-            break;
+                break;
             /**
-             * 5XY0 skip next instruction if VX == VY
+             * 5XY0-skip next instruction if VX == VY
              */
-            case 0x5000:
+            case 0x5:
                 skipInstr("5XY0");
-            break;
+                break;
 
             /**
+             * 6XKK
              * set VX = KK
              * The interpreter puts the value KK into register VX.
              */
-            case 0x6000:
+            case 0x6:
                 putKKintoVX();
-            break;
+                break;
             /**
+             * 7XKK
              * Set VX = VX + KK.
              *Adds the value kk to the value of register VX, then stores the result in VX.
              */
-            case 0x7000:
+            case 0x7:
                 addValueToRegister();
-            break;
+                break;
 
+            case 0x8:
+                switch (opcode & 0x000F) {
+                    /**
+                     * Store the value of register VY in register VX
+                     */
+                    case 0x0:
+                        registerVYinVX();
+                        break;
+                    /**
+                     * 8XY1
+                     * Set VX to VX or VY
+                     */
+                    case 0x1:
+                        logicalOR();
+                        break;
+                    /**
+                     * 8XY2
+                     * Set VX to VX and VY
+                     */
+                    case 0x2:
+                        logicalAND();
+                        break;
+                    /**
+                     * 8XY3
+                     *Set VX to VX XOR VY
+                     */
+                    case 0x3:
+                        logicalXOR();
+                        break;
+                    /**
+                     *8XY4
+                     *Add the value of register VY to register VX
+                     *Set VF to 01 if a carry occurs
+                     *Set VF to 00 if a carry does not occur
+                     */
+                    case 0x4:
+                        addRegisterToRegister();
+                        break;
 
+                }
+                break;
             /**
              * The value of register indexRegister is set to nnn
              */
@@ -184,6 +227,58 @@ public class Memory {
     }
 
     /**
+     * Similar to the 7XKK instruction, the 8XY4 instruction will cause values too large to be stored in a register
+     * to be wrapped around to a modulo equivelent. However, unlike the 7XNN instruction, the 8XY4 instruction will
+     * modify the VF register to signal when a carry has taken place. A carry is a term used to describe
+     * the aforementioned action of when a value is too large to be stored in a given register.
+     * When a carry takes place, the interpreter will set register VF to 01.
+     * Otherwise, VF will be set to 00. Therefore, the 8XY4 instruction will always modify the VF register.
+     *
+     * notice from:
+     * http://mattmik.com/files/chip8/mastering/chip8.html
+     */
+
+    private void addRegisterToRegister() {
+        int temp = V[(opcode & 0x0F00) >> 8] + V[(opcode & 0x00F0) >> 4];
+
+        if(temp > 255) {
+            V[(opcode & 0x0F00) >> 8] = temp - 256;
+            V[0xF] = 1;
+        }
+        else {
+            V[(opcode & 0x0F00) >> 8] = temp;
+            V[0xF] = 0;
+        }
+        pc += 2;
+
+    }
+    private void logicalXOR() {
+        V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
+
+    }
+
+    private void logicalAND() {
+        V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
+        pc =+ 2;
+
+    }
+
+    private void logicalOR() {
+        /**
+         * a = a | b == a |= b
+         */
+        V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
+        pc =+ 2;
+    }
+
+
+    private void registerVYinVX() {
+        V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+        pc += 2;
+    }
+
+
+    /**
      * Be aware that once the supplied number is added, if the value of the register exceeds decimal 255
      * (the highest possible value that can be stored by an eight bit register), the register will wraparound to a
      * corresponding value that can be stored by an eight bit register. In other words, the register will always be
@@ -192,12 +287,10 @@ public class Memory {
      * notice from:
      * http://mattmik.com/files/chip8/mastering/chip8.html
      */
-
     private void addValueToRegister() {
         int result = V[(opcode & 0x0F00) >> 8] + (opcode & 0x00FF);
-        V[(opcode & 0x0F00) >> 8] = (result > 256) ? result - 256 : result;
+        V[(opcode & 0x0F00) >> 8] = (result > 255) ? result - 256 : result;
         pc += 2;
-
     }
 
     private void putKKintoVX() {

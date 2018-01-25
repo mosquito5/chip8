9e9e656dd172640fdfca1e5d0de924a8fec6f848
Jakub <jakub.moskal3@gmail.com>
2018-01-25 13:00:10 +0100

    generated files on 2018-01-25 13:00:10

--- src/main/java/pl/mosquito/chip8/emulator/Memory.java
+++ src/main/java/pl/mosquito/chip8/emulator/Memory.java
@@ -7,30 +7,30 @@ public class Memory {
 
     //The default size of memory
 
-    private final int MEMORY_4K = 0x1000; //4096
-    private final int REG_SIZE = 16;
+    private final int   MEMORY_4K = 0x1000; //4096
+    private final int   REG_SIZE = 16;
 
-    private int opcode;
+    private int         opcode;
 
-    private short[] memory;
-    private int[] V;
+    private short[]     memory;
+    private int[]       V;
 
-    private int indexRegister;
-    private int pc; // program counter whitch can contain a value from 0x000 to 0xFFF
+    private int         I;  //index register
+    private int         pc; // program counter whitch can contain a value from 0x000 to 0xFFF
 
     /**
      * timer registers that counts at 60Hz.
      * When set above 0 they will count down to 0
      */
-    private int delayTimer;
-    private int soundTimer; // The system's buzzer sounds whenever the sound timer reaches 0
+    private int         delayTimer;
+    private int         soundTimer; // The system's buzzer sounds whenever the sound timer reaches 0
 
     /**
      *@param stack - Remembers location between jumps
      *@param sp - Remember which level of stack was used.
      */
-    private int stack[];
-    private byte sp;
+    private int         stack[];
+    private byte        sp;
 
     private Screen      screen;
     //private Keyboard    keyboard;
@@ -42,10 +42,10 @@ public class Memory {
         V               = new int[REG_SIZE];
         stack           = new int[REG_SIZE];
 
-        pc              = 0x200; //Program counter starts at 0x200
+        /*pc              = 0x200; //Program counter starts at 0x200
         opcode          = 0; //Reset current opcode
-        indexRegister   = 0;
-        sp              = 0; // Reset stack pointer
+        I               = 0;
+        sp              = 0; // Reset stack pointer*/
 
         this.screen     = screen;
         //keyboard        = new Keyboard();
@@ -63,21 +63,21 @@ public class Memory {
     private void reset() {
         //Resets the memory, register and stack
         for (int i = 0; i < memory.length; i++) {
-            memory[i] = 0;
+            memory[i]       = 0;
             if(i < REG_SIZE) {
                 V[i]        = 0;
                 stack[i]    = 0;
             }
         }
         screen.clear();      // Reset the screen
-        delayTimer = 0;
-        soundTimer = 0;
+        delayTimer          = 0;
+        soundTimer          = 0;
     }
 
     private void init() {
         pc              = 0x200; //Program counter starts at 0x200
         opcode          = 0; //Reset current opcode
-        indexRegister   = 0;
+        I               = 0;
         sp              = 0; // Reset stack pointer
     }
 
@@ -131,31 +131,31 @@ public class Memory {
                  * jump to address NNN
                  */
             case 0x1:
-                jumpToAddr(opcode);
+                jumpToAddr();
                 break;
             /**
              * 2NN-jump to subroutine at address NNN
              */
             case 0x2:
-                jumpToSubroutineAddr(opcode);
+                jumpToSubroutineAddr();
                 break;
             /**
              * 3XKK-skip next instruction if reguster VX == register KK
              */
             case 0x3:
-                skipInstr("3XKK");
+                skipInstr();
                 break;
             /**
              * 4XKK-skip next instruction if reguster VX != register KK
              */
             case 0x4:
-                skipInstr("4XKK");
+                skipInstr();
                 break;
             /**
              * 5XY0-skip next instruction if VX == VY
              */
             case 0x5:
-                skipInstr("5XY0");
+                skipInstr();
                 break;
 
             /**
@@ -213,17 +213,113 @@ public class Memory {
                     case 0x4:
                         addRegisterToRegister();
                         break;
-
+                    /**
+                     *8XY5
+                     *Subtract the value of register VY from register VX
+                     *Set VF to 00 if a borrow occurs
+                     *Set VF to 01 if a borrow does not occur
+                     */
+                    case 0x5:
+                        subtractRegisterFromRegister();
+                        break;
+                    /**
+                     * 8X06
+                     *shift register VX right, bit 0 goes int register VF
+                     */
+                    case 0x6:
+                        rightShift();
+                        break;
+                    /**
+                     * 8XY7
+                     * Set register VX to the value of VY minus VX
+                     *Set VF to 00 if a borrow occurs
+                     *Set VF to 01 if a borrow does not occur
+                     */
+                    case 0x7:
+                        subtractRegisterFromRegister();
+                        break;
+                    /**
+                     * 8X0E
+                     * shift register VX left, bit 7 stored into register VF
+                     */
+                    case 0xE:
+                        leftShift();
+                        break;
                 }
                 break;
             /**
+             * 9XY0
+             * skip next instruction if register VX != register VY
+             */
+            case 0x9:
+                skipInstr();
+                break;
+
+            /**
              * The value of register indexRegister is set to nnn
              */
-            case 0xA000:
-                idndexRegisterSetTo(opcode);
+            case 0xA:
+                indexRegisterSetTo();
             break;
+            /**
+             * BNNN
+             * jump to address NNN + V0
+             */
+            case 0xB:
+                jumpToAddr();
+                break;
 
         }
+    }
+
+    private void leftShift() {
+        int sourceRegister  =    (opcode & 0x0F00)          >>     8;
+        V[0xF]              =    (V[sourceRegister] >> 7)   ==     1 ? 1 : 0;
+        V[sourceRegister]   =    V[sourceRegister]          <<     1;
+
+        pc += 2;
+    }
+
+    private void rightShift() {
+        int sourceRegister  =    (opcode & 0x0F00)      >>     8;
+        V[0xF]              =    (V[sourceRegister])    ==     1 ? 1 : 0;
+        V[sourceRegister]   =    V[sourceRegister]      >>     1;
+
+        pc += 2;
+
+    }
+
+    private void subtractRegisterFromRegister() {
+        int temp = -300;
+
+        switch (opcode & 0x000F) {
+
+            case 0x5:
+                 temp = V[(opcode & 0x0F00) >> 8] - V[(opcode % 0x00F0) >> 4];
+                 break;
+
+            case 0x7:
+                 temp = V[(opcode % 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
+
+                break;
+        }
+
+        if (temp < 0) {
+            //sets VF = 0 when borrow occurs
+            V[0xF]                      = 0;
+            V[(opcode & 0xF00) >> 8]    = temp  + 256;
+            pc                          += 2;
+        }
+        else {
+            V[0xF]                      = 1;
+            V[(opcode & 0xF00) >> 8]    = temp;
+
+            pc                          += 2;
+        }
+
+        if(temp == -300)
+            System.out.println("subtractRegisterFromRegister() ERROR");
+
     }
 
     /**
@@ -242,24 +338,25 @@ public class Memory {
         int temp = V[(opcode & 0x0F00) >> 8] + V[(opcode & 0x00F0) >> 4];
 
         if(temp > 255) {
-            V[(opcode & 0x0F00) >> 8] = temp - 256;
-            V[0xF] = 1;
+            V[(opcode & 0x0F00) >> 8]   = temp - 256;
+            V[0xF]                      = 1;
         }
         else {
-            V[(opcode & 0x0F00) >> 8] = temp;
-            V[0xF] = 0;
+            V[(opcode & 0x0F00) >> 8]   = temp;
+            V[0xF]                      = 0;
         }
         pc += 2;
 
     }
     private void logicalXOR() {
-        V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
+        V[(opcode & 0x0F00) >> 8]   ^= V[(opcode & 0x00F0) >> 4];
+        pc                          += 2;
 
     }
 
     private void logicalAND() {
-        V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
-        pc =+ 2;
+        V[(opcode & 0x0F00) >> 8]   &= V[(opcode & 0x00F0) >> 4];
+        pc                          += 2;
 
     }
 
@@ -267,14 +364,14 @@ public class Memory {
         /**
          * a = a | b == a |= b
          */
-        V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
-        pc =+ 2;
+        V[(opcode & 0x0F00) >> 8]   |= V[(opcode & 0x00F0) >> 4];
+        pc                          += 2;
     }
 
 
     private void registerVYinVX() {
-        V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
-        pc += 2;
+        V[(opcode & 0x0F00) >> 8]   = V[(opcode & 0x00F0) >> 4];
+        pc                          += 2;
     }
 
 
@@ -288,55 +385,70 @@ public class Memory {
      * http://mattmik.com/files/chip8/mastering/chip8.html
      */
     private void addValueToRegister() {
-        int result = V[(opcode & 0x0F00) >> 8] + (opcode & 0x00FF);
-        V[(opcode & 0x0F00) >> 8] = (result > 255) ? result - 256 : result;
-        pc += 2;
+        int result                  = V[(opcode & 0x0F00) >> 8] + (opcode & 0x00FF);
+        V[(opcode & 0x0F00) >> 8]   = (result > 255) ? result - 256 : result;
+        pc                          += 2;
     }
 
     private void putKKintoVX() {
-        V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF);
-        pc +=2;
+        V[(opcode & 0x0F00) >> 8]   = (opcode & 0x00FF);
+        pc                          +=2;
     }
 
-    private void skipInstr(String instruction) {
-        switch (instruction) {
-            case "3XKK":
-                if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
-                    pc += 4;
+    private void skipInstr() {
+        switch (opcode & 0xF000) {
+            case 0x3:
+                if (V[(opcode & 0x0F00) >> 8]   == (opcode & 0x00FF))
+                    pc                          += 4;
                 else
-                    pc += 2;
+                    pc                          += 2;
                 break;
 
-            case "4XKK":
-                if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
-                    pc += 4;
+            case 0x4:
+                if (V[(opcode & 0x0F00) >> 8]   != (opcode & 0x00FF))
+                    pc                          += 4;
                 else
-                    pc += 2;
-            break;
+                    pc                          += 2;
+                break;
 
-            case "5XY0" :
-                if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00F0) >> 4)
-                    pc += 4;
+            case 0x5:
+                if (V[(opcode & 0x0F00) >> 8]   == V[(opcode & 0x00F0) >> 4])
+                    pc                          += 4;
                 else
-                    pc += 2;
+                    pc                          += 2;
+                break;
+            case 0x9:
+                if (V[(opcode & 0x0F00) >> 8]   != V[(opcode & 0x00F0) >> 4])
+                    pc                          += 4;
+                else
+                    pc                          += 2;
+                break;
 
-            break;
         }
     }
 
-    private void jumpToSubroutineAddr(int opcode) {
-        stack[sp] = pc;
+    private void jumpToSubroutineAddr() {
+        stack[sp]   = pc;
         sp++;
-        pc =        (opcode & 0x0FFF);
+        pc          = (opcode & 0x0FFF);
     }
 
-    private void jumpToAddr(int opcode) {
-        pc = (opcode & 0x0FFF);
+    private void jumpToAddr() {
+        switch (opcode & 0xF) {
+            case 0x1:
+                pc  = (opcode & 0x0FFF);
+                break;
+
+            case 0xB:
+                pc  = (opcode & 0x0FFF) + V[0];
+                break;
+
+        }
     }
 
-    private void idndexRegisterSetTo(int opcode) {
-        indexRegister = (opcode & 0x0FFF);
-        pc += 2;
+    private void indexRegisterSetTo() {
+        I               = (opcode & 0x0FFF);
+        pc              += 2;
     }
 
     private void scrClr() {
@@ -345,8 +457,8 @@ public class Memory {
     }
 
     private void returnFromSubroutine() {
-        pc =    stack[sp--];
-        pc +=   2;
+        pc      = stack[sp--];
+        pc      += 2;
     }
 }
 

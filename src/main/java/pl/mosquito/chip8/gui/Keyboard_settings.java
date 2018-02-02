e35f6d5da6745c627fca67098d0954186f086e9c
Jakub <jakub.moskal3@gmail.com>
2018-02-02 13:37:49 +0100

    generated files on 2018-02-02 13:37:49

--- src/main/java/pl/mosquito/chip8/gui/Keyboard_settings.java
+++ src/main/java/pl/mosquito/chip8/gui/Keyboard_settings.java
@@ -6,9 +6,12 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+
 
 
 public class Keyboard_settings  {
@@ -24,11 +27,15 @@ public class Keyboard_settings  {
     private Button              saveSetButton;
     private Button              closeButton;
     private Buttons             buttons;
+    private Labels              labels;
+    private Label[]             label;
+    private TextField[]         textFields;
 
     public Keyboard_settings() {
         thread = new Thread( "chip8 keyboard settings");
         thread.start();
         window();
+
 
     }
 
@@ -36,6 +43,9 @@ public class Keyboard_settings  {
         System.out.println("Keyboard_settings");
 
         buttons         = new Buttons();
+        labels          = new Labels();
+        label           = new Label[16];
+        textFields      = new TextField[16];
         primarystage    = new Stage();
         borderPane      = new BorderPane();
         flowPaneLeft    = new FlowPane();
@@ -44,12 +54,23 @@ public class Keyboard_settings  {
         DoneButton      = buttons.Done();
         saveSetButton   = buttons.saveSet();
         closeButton     = buttons.closeKeyboard();
+        setUI();
         setPane();
         primarystage.setTitle("Keyboard settings");
+        primarystage.setResizable(false);
 
-        scene         = new Scene(borderPane);
+
+        scene         = new Scene(borderPane,260,470);
         primarystage.setScene(scene);
         primarystage.show();
+    }
+
+    private void setUI() {
+        for(int element = 0; element < label.length; element++) {
+            label[element]      = new Label(labels.getList(element));
+            textFields[element] = new TextField(labels.getList(element));
+            textFields[element].setPrefWidth(25);
+        }
     }
 
     private void setPane() {
@@ -57,9 +78,16 @@ public class Keyboard_settings  {
         borderPaneSet();
     }
 
+    /**
+     * temporary solution
+     */
     private void flowPaneSet() {
         flowPaneLeft.setOrientation(Orientation.VERTICAL);
+        flowPaneLeft.setVgap(10.48);
+        flowPaneLeft.getChildren().addAll(label);
+
         flowPaneRight.setOrientation(Orientation.VERTICAL);
+        flowPaneRight.getChildren().addAll(textFields);
 
         flowPaneBottom.setAlignment(Pos.BOTTOM_RIGHT);
         flowPaneBottom.setHgap(10);
--- src/main/java/pl/mosquito/chip8/gui/Labels.java
+++ src/main/java/pl/mosquito/chip8/gui/Labels.java
@@ -1,88 +1,23 @@
 package pl.mosquito.chip8.gui;
 
-import javafx.scene.control.Label;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Labels {
+    private List<String> labList = new ArrayList<String>();
 
-    Label zero () {
-        Label label = new Label("0");
-        return label;
+    Labels() {
+        filLablList();
     }
 
-    Label one () {
-        Label label = new Label("1");
-        return label;
+    private void filLablList() {
+        labList.addAll(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7",
+                "8", "9", "A", "B", "C", "D", "E", "F"));
     }
 
-    Label two () {
-        Label label = new Label("2");
-        return label;
+    public String getList(int element) {
+        return labList.get(element);
     }
-
-    Label three () {
-        Label label = new Label("3");
-        return label;
-    }
-
-    Label four () {
-        Label label = new Label("4");
-        return label;
-    }
-
-    Label five () {
-        Label label = new Label("5");
-        return label;
-    }
-
-    Label six () {
-        Label label = new Label("6");
-        return label;
-    }
-
-    Label seven () {
-        Label label = new Label("7");
-        return label;
-    }
-
-    Label eight () {
-        Label label = new Label("8");
-        return label;
-    }
-
-    Label nine () {
-        Label label = new Label("9");
-        return label;
-    }
-
-    Label A () {
-        Label label = new Label("A");
-        return label;
-    }
-
-    Label B () {
-        Label label = new Label("B");
-        return label;
-    }
-
-    Label C () {
-        Label label = new Label("C");
-        return label;
-    }
-
-    Label D () {
-        Label label = new Label("D");
-        return label;
-    }
-
-    Label E () {
-        Label label = new Label("E");
-        return label;
-    }
-
-    Label F () {
-        Label label = new Label("F");
-        return label;
-    }
-
 
 }
